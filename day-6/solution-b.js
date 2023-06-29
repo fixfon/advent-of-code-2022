adbe9652128dd325dc5cb2310f610de810e93c86
Fixfon <12512757+fixfon@users.noreply.github.com>
2023-06-29 20:42:07 +0300

    Day 6 Part two

---
+++ day-6/solution-b.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile(path.join(__dirname, 'input.txt'), 'utf8', (err, data) => {
+	if (err) throw err;
+	for (let i = 0; i < data.length; i++) {
+		if (i < 13) continue;
+		const fourStack = data.slice(i - 13, i + 1);
+		if (
+			!fourStack
+				.split('')
+				.some((char, index) => fourStack.indexOf(char) !== index)
+		) {
+			console.log(i + 1);
+			break;
+		}
+	}
+});
